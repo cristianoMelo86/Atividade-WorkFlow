04fc4f17d06f708948fa6246d37f8330ca2d542a
Cristiano <flashbass19@gmail.com>
2017-11-14 17:36:49 -0200

    só falta criar as tarefas do gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,33 @@
-var gulp = require("gulp")
+var gulp = require("gulp");
+var scss = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+
+
+// FN Transforma Scss em Css
+gulp.task('scss', function () {
+   	 return gulp.src('./source/scss/*.scss')
+        .pipe(scss({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./css'));
+	});
+
 
 // FN Mover arquivo
-gulp.task('mover-arquivo', function(){
+gulp.task('move-css', function(){
 	return gulp.src("./source/scss/*.scss")
 		   .pipe(gulp.dest("./dist/css"));
 });
 
+// FN Minificar Css
+gulp.task('min', function () {
+    gulp.src('./dist/*.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('./dist/css/'));
+});
+
 
 //FN Monitorar arquivo
-gulp.task('monitorar-arquivo', function(){
-	gulp.watch('./source/scss/*.scss', ['mover-arquivo']);
+gulp.task('background', function(){
+	gulp.watch('./source/scss/*.scss', ['move-css']);
 });
+
+
